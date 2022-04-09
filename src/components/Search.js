39b8ba0f2c3d4fa77bb9a845d07c52710d0ff41a
Measum Abbas <maisumabbas.charbu@gmail.com>
2022-04-09 21:17:22 +0500

    single blog created

---
+++ src/components/Search.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Search = ({handleSearch, searchValue, onInputChange}) => {
+  return (
+    <div className='search-form'>
+        
+    </div>
+  )
+}
+
+export default Search
--- src/pages/Blog.js
+++ src/pages/Blog.js
@@ -7,6 +7,7 @@ import Badge from '../components/Badge';
 const Blog = () => {
 
   const [blog, setBlog] = useState();
+  const [relatedPost, setRelatedPost] = useState([])
   const { id } = useParams();
 
   const navigate = useNavigate()
@@ -16,35 +17,41 @@ const Blog = () => {
     }
   }, [id])
 
-  const loadSingleBlog = async (id) => {
+  const loadSingleBlog = async () => {
     const response = await axios.get(`http://localhost:500/blogs/${id}`);
-    if (response.status === 200) {
+    const relatedPosts = await axios.get(`http://localhost:500/blogs?category=${response.data.catagory}&_start=0&_end=3`);
+    console.log(relatedPosts.data);
+
+    if (response.status === 200 || relatedPosts.status === 200) {
       setBlog(response.data);
+      setRelatedPost(relatedPosts.data)
 
     } else {
       toast.error('something went wrong');
+
     }
   }
-  // http://res.cloudinary.com/drkf8to4g/image/upload/v1649260554/vfe1qvwvvr5gmxu3dgur.png
+
+
   const handleBack = ()=>{
     navigate('/')
   }
-
+  
   return (
     <div className='container'>
-      {/* <div className="card my-4">
+      <div className="card my-4">
         <span className='btn btn-primary' onClick={handleBack}>Go Back</span>
             <h5 style={{fontSize:'30px',padding:'10px'}} className="card-title text-center">{blog && blog.title}</h5>
-        <img src={blog.imageUrl} className="card-img-top" alt="image" style={{height:'400px'}}/>
+        <img src={blog && blog.imageUrl} className="card-img-top" alt="image" style={{height:'400px'}}/>
           <div className="card-body">
             <div className="d-flex justify-content-between p-2">
-              <p>{blog.date}</p>
-              <Badge children={blog.catagory}/>
+              <p>{blog && blog.date}</p>
+              <Badge children={blog && blog.catagory}/>
             </div>
-            <p className="card-text text-center">{blog.description}</p>
+            <p className="card-text text-center">{blog && blog.description}</p>
             
           </div>
-      </div> */}
+      </div>
     </div>
   )
 }
